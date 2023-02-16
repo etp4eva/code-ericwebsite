35c7fa27c15395160ad3234a735652127e68e096
etp4eva <eric@ericwebsite.info>
2023-02-16 10:27:37 +0100

    Sneaky email trick

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,37 @@
 import styles from '@/styles/Footer.module.scss'
+import { MouseEventHandler, useRef } from 'react'
 
 export const Footer = () => {
+    
+    const onClick: MouseEventHandler<HTMLElement> = (context) => {
+        const t = context.currentTarget;
+        window.open(`mailto:${t.dataset.name}@${t.dataset.domain}.${t.dataset.tld}`);
+        return false;
+    }
+
+    const onMouseEnter: MouseEventHandler<HTMLElement> = (context) => {
+        const t = context.currentTarget;
+        t.textContent = `${t.dataset.name}@${t.dataset.domain}.${t.dataset.tld}`;
+    }
+
+    const onMouseLeave: MouseEventHandler<HTMLElement> = (context) => {
+        const t = context.  currentTarget;
+        t.textContent = `Email Me`;
+    }
+        
     return (
         <footer className={ styles.footer }>
             <div>
-                <a href='mailto:eric@ericwebsite.info'>eric@ericwebsite.info</a>
+                <span 
+                    data-name="eric" 
+                    data-domain="ericwebsite" 
+                    data-tld="info" 
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                    onClick={onClick}
+                >
+                    Email Me
+                </span>
             </div>
             <div></div>
             <div>☺</div>
