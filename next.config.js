b52eff85dd924b5c2894d39ea5325d358af1b33e
etp4eva <eric@ericwebsite.info>
2023-02-27 14:58:02 +0100

    docker stuff

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-        config.resolve.fallback = {
-            fs: false
+    reactStrictMode: true,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+            config.resolve.fallback = {
+                fs: false
+            }
         }
-    }
 
-    return config;
-}
+        return config;
+    },
+    output: 'standalone',
 }
 
 module.exports = nextConfig
